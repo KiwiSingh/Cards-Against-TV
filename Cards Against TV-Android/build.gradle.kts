dd92d8f6942d83973800a5be565d8a626b4fb79e
Kiwi Singh <metal.garuruwind.tetusaiga223@gmail.com>
2025-08-31 23:38:36 +0530

    Add files via upload

---
+++ Cards Against TV-Android/build.gradle.kts
@@ -0,0 +1,9 @@
+// Top-level build file
+
+plugins {
+    id("com.android.application") version "8.12.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
+    // ADD THIS LINE FOR COMPOSE COMPILER PLUGIN
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" apply false
+}
